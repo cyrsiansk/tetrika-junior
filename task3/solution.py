a7f83b7794d1de3474c3422a0d8869aa7a9620b0
cyrsiansk <cyrsiadv@gmail.com>
2025-06-14 01:20:33 +0400

    Я забыл это убрать😭

--- task3/solution.py
+++ task3/solution.py
@@ -55,11 +55,3 @@ def appearance(data: dict[str, list[int]]) -> int:
             session.leave(member, timestamp)
 
     return session.counter
-
-data = {
-    'lesson': [1594663200, 1594666800],
-    'pupil': [1594663340, 1594663389, 1594663390, 1594663395, 1594663396, 1594666472],
-    'tutor': [1594663290, 1594663430, 1594663443, 1594666473]
-}
-
-print(appearance(data))
